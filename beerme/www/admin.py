2d5d150145c432e32bb3aca838723cb0ba0617b6
me <me@robertcurtiss.com>
2023-03-06 22:13:07 -0500

    Monday, added races

--- beerme/www/admin.py
+++ beerme/www/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from .models import Team, Driver, Track, Race, Person, Bet, Tv
+
 # Register your models here.
+
+
+class BetAdmin(admin.ModelAdmin):
+    list_display = [
+        "beer",
+        "person_id",
+        "race_id",
+        "driver_id",
+        "finish",
+    ]
+
+
+admin.site.register(Team)
+admin.site.register(Driver)
+admin.site.register(Track)
+admin.site.register(Race)
+admin.site.register(Person)
+admin.site.register(Tv)
+admin.site.register(Bet, BetAdmin)
--- beerme/www/models.py
+++ beerme/www/models.py
@@ -1,26 +1,87 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import datetime as dt
+from django.utils import timezone
 
 # Create your models here.
 
 
-class Person(models.Model):
-    name = models.CharField(max_length=32)
+class base(models.Model):
+    now = timezone.datetime
+    createdAt = models.DateTimeField("date created", auto_now=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    updatedAt = models.DateTimeField("date last updated", auto_now_add=True, null=True)
+
+    class Meta:
+        abstract = True
 
 
-class Track(models.Model):
-    name = models.CharField(max_length=32)
+class Person(base):
+    name = models.CharField(max_length=32, default="")
+
+    def __str__(self) -> str:
+        return self.name
 
 
-class Driver(models.Model):
-    name = models.CharField(max_length=32)
+class Track(base):
+    name = models.CharField(max_length=32, default="", unique=True)
+
+    def __str__(self) -> str:
+        return f"{self.name:<32}"
 
 
-class Race(models.Model):
-    track_id = models.ForeignKey(Track, on_delete=models.CASCADE)
-    race_date = models.DateField()
+class Team(base):
+    name = models.CharField(max_length=32, default="", unique=True)
+
+    def __str__(self) -> str:
+        return self.name
 
 
-class Bet(models.Model):
-    race_date = models.DateField()
-    person_name = models.ForeignKey(Person, on_delete=models.CASCADE)
+class Driver(base):
+    name = models.CharField(max_length=32, default="", unique=True)
+    team_id = models.ForeignKey(Team, models.CASCADE, default=-1)
+
+    def __str__(self) -> str:
+        return f"{self.name:32} - Team {self.team_id}"
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Tv(models.Model):
+    name = models.CharField(max_length=24, default="FOX", null=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Race(base):
+    track_id = models.ForeignKey(Track, on_delete=models.CASCADE, default=-1)
+    race_date = models.DateField(default=dt.date.today, unique=True)
+    tv_id = models.ForeignKey(Tv, on_delete=models.CASCADE, null=True)
+    tv = models.CharField(max_length=32, default="FOX")
+
+    def __str__(self) -> str:
+        return f"{self.race_date} - {self.track_id} - {self.tv_id}"
+
+    class Meta:
+        ordering = ["race_date"]
+
+
+class Bet(base):
+    race_id = models.ForeignKey(Race, on_delete=models.CASCADE, default=-1)
+    person_id = models.ForeignKey(Person, on_delete=models.CASCADE, default=-1)
+    driver_id = models.ForeignKey(Driver, on_delete=models.CASCADE, default=-1)
+    finish = models.IntegerField(default=-1)
+    beer = models.BooleanField(default=False)
+    bet_str = f"{race_id}"
+
+    def __str__(self) -> str:
+        if self.beer:
+            award = " --- BEER"
+        else:
+            award = ""
+        return f"{self.person_id} - {self.race_id} - {self.driver_id} Finshed={self.finish} {award} {self.bet_str}"
+
+    class Meta:
+        ordering = ["race_id", "person_id"]
